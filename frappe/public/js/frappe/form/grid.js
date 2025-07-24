69fe5c6dde0f610af42a3c8307883d81e12f2d33
Adam Wu <adam.wu@trinfo.net>
2025-07-24 01:08:13 +0000

    feat: 调整grid自定义按钮的顺序

--- frappe/public/js/frappe/form/grid.js
+++ frappe/public/js/frappe/form/grid.js
@@ -1175,7 +1175,7 @@ export default class Grid {
 		if (!$btn) {
 			$btn = $(`<button type="button" class="btn btn-secondary btn-xs btn-custom">`)
 				.html(__(label))
-				.prependTo($wrapper)
+				.appendTo($wrapper)
 				.on("click", click);
 			this.custom_buttons[label] = $btn;
 		} else {
